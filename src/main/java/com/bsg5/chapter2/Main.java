be38b383df56b631a16045e1119ebac5d7599d38
antoniocassano84 <antonio.cassano984@gmail.com>
2020-06-15 20:35:36 +0100

    Initial commit

---
+++ src/main/java/com/bsg5/chapter2/Main.java
@@ -0,0 +1,17 @@
+package com.bsg5.chapter2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+    ApplicationContext context =
+            new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    Greeter greeter = context.getBean("helloGreeter", Greeter.class);
+    greeter.setPrintStream(System.out);
+    greeter.greet();
+  }
+}
